5b0176b27a03dcd984a07638ec70e78b7cab24d3
Brian Yingling <brian@jet.com>
2014-12-24 12:22:11 -0500

    Imported styles and scripts

---
+++ SimpleBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace SimpleBlog.App_Start
+
+    // comes with a script bundle and a style bundle
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles) {
+            bundles.Add(new StyleBundle("~/admin/styles")
+                    .Include("~/Content/Styles/bootstrap.css")
+                    .Include("~/Content/Styles/admin.css"));
+
+            bundles.Add(new StyleBundle("~/Styles")
+                    .Include("~/Content/Styles/bootstrap.css")
+                    .Include("~/Content/Styles/Site.css"));
+
+            bundles.Add(new ScriptBundle("~/admin/scripts")
+                    .Include("~/Scripts/jquery-1.8.0.js")
+                    .Include("~/Scripts/jquery.validate.js")
+                    .Include("~/Scripts/jquery.validate.unobtrusive.js")
+                    .Include("~/Scripts/bootstrap.js"));
+
+
+            bundles.Add(new ScriptBundle("~/scripts")
+                    .Include("~/Scripts/jquery-1.8.0.js")
+                    .Include("~/Scripts/jquery.validate.js")
+                    .Include("~/Scripts/jquery.validate.unobtrusive.js")
+                    .Include("~/Scripts/bootstrap.js"));
+               
+
+        }
+    }
+}
