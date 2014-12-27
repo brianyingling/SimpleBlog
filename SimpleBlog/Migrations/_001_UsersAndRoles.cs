26eff2b416699559891c8ccb6cc74c626a89cb11
Brian Yingling <brian@jet.com>
2014-12-27 16:46:07 -0500

    added db migration files

---
+++ SimpleBlog/Migrations/_001_UsersAndRoles.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using FluentMigrator;
+
+namespace SimpleBlog.Migrations
+{
+    [Migration(1)]
+    public class _001_UsersAndRoles : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("users")
+                .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("username").AsString(128)
+                .WithColumn("email").AsCustom("VARCHAR(256)")
+                .WithColumn("password_hash").AsString(128);
+
+            Create.Table("roles")
+                .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("name").AsString(128);
+
+            // many-to-many relationship
+            // referential integrity -- must have a valid user id
+            // on delete -- if a user is deleted, also delete corresponding table(s) in role_users
+            // on delete -- if a role is deleted, also delete corresponding table(s) in role_users
+            Create.Table("role_users")
+                .WithColumn("user_id").AsInt32().ForeignKey("users", "id").OnDelete(Rule.Cascade)
+                .WithColumn("role_id").AsInt32().ForeignKey("roles", "id").OnDelete(Rule.Cascade);
+
+        }
+
+        public override void Down()
+        {
+            Delete.Table("role_users");
+            Delete.Table("roles");
+            Delete.Table("users");
+        }
+    }
+}
---
+++ SimpleBlog/Tools/DeployDb-Dev.bat
@@ -0,0 +1,1 @@
+..\..\packages\FluentMigrator.1.3.1.0\tools\Migrate.exe --db=mysql --target=..\bin\SimpleBlog.dll --configPath=..\Web.config -c=MainDb
