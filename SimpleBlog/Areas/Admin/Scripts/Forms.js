828a14e181ccfcb95fcff50516f2ca1f4ab368a5
Brian Yingling <brian@jet.com>
2015-01-25 15:27:48 -0500

    Delete, trash, and restore posts; also slugifying component

--- SimpleBlog/Areas/Admin/Scripts/Forms.js
+++ SimpleBlog/Areas/Admin/Scripts/Forms.js
@@ -16,7 +16,22 @@
             .append(antiForgeryInput)
             .appendTo(document.body)
             .submit();
-
     });
 
+    $("[data-slug]").each(function () {
+        var $this = $(this);
+        var $sendSlugFrom = $($this.data("slug"));
+
+        $sendSlugFrom.keyup(function () {
+            var slug = $sendSlugFrom.val();
+            slug = slug.replace(/[^a-zA-Z0-9\s]/g, "");
+            slug = slug.toLowerCase();
+            slug = slug.replace(/\s+/g, "-");
+            
+            if (slug.charAt(slug.length - 1) == "-")
+                slug.substr(0, slug.length - 1);
+            
+            $this.val(slug);
+        });
+    });
 });
