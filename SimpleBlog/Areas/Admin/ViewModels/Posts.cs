2ad96e3fe15f9008fd58c52e45a0685fa2ecfa74
Brian Yingling <brian@jet.com>
2015-01-25 14:23:29 -0500

    created new and edit actions for the posts controller

--- SimpleBlog/Areas/Admin/ViewModels/Posts.cs
+++ SimpleBlog/Areas/Admin/ViewModels/Posts.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
 using SimpleBlog.Infrastructure;
 using SimpleBlog.Models;
 
@@ -8,4 +9,20 @@ namespace SimpleBlog.Areas.Admin.ViewModels
     {
         public PagedData<Post> Posts { get; set; }
     }
+
+    public class PostsForm 
+    {
+        public bool IsNew { get; set; }
+        public int? PostId { get; set; }
+        
+        [Required, MaxLength(128)]
+        public string Title { get; set; }
+
+        [Required, MaxLength(128)]
+        public string Slug { get; set; }
+
+        [Required, DataType(DataType.MultilineText)]
+        public string Content { get; set; }
+    }
+
 }
